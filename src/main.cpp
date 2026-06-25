27a80cf715603446fde103859de1b5f7755f5e07
TRUONG HOANG TIN <hoangtin285@gmail.com>
2026-06-25 08:11:11 +0700

    init project folder

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include <QWidget>
+
+int main(int argc, char* argv[]) {
+    QApplication a(argc, argv);
+
+    QWidget window;
+    window.setWindowTitle("Management-App");
+    window.resize(800, 600);
+    window.show();
+
+    return a.exec();
+}
