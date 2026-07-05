48938eac38d2ef63b5a9b5df647de39e107d96e1
HwngMP <pmai4118@gmail.com>
2026-07-05 20:24:35 +0700

    merge from tin

--- src/core/Staff.cpp
+++ src/core/Staff.cpp
@@ -1,19 +1,11 @@
 #include "core/Staff.h"
 
-Staff::Staff(QString r, short int idEmp, QString ava, QString idCit, QString n, QString d, QString add, QString phone) : User(r, idEmp, ava, idCit, n, d, add, phone)
+Staff::Staff(QString r, short int idEmp, QString ava, QString idCit, QString n, QString d, QString add, QString phone)
+	: User(r, idEmp, ava, idCit, n, d, add, phone)
 {
-<<<<<<< Updated upstream
-	Staff::Staff(QString r, short int idEmp, QString ava, QString idCit, QString n, QString d, QString add, QString phone) : User(r, idEmp, ava, idCit, n, d, add, phone)
-	{
+}
 
-		== == == =
-					 Staff::Staff(QString r, short int idEmp, QString ava, QString idCit, QString n, QString d, QString add, QString phone, QString gender) : User(r, idEmp, ava, idCit, n, d, add,
-																																								   phone, gender)
-		{
->>>>>>> Stashed changes
-		}
-
-		double Staff::getSalary() const
-		{
-			return 0.0;
-		}
+double Staff::getSalary() const
+{
+	return 0.0;
+}
