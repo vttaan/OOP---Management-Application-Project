4fbbabd98f94a48a80f91678aadfbbc5ead4e40b
TRUONG HOANG TIN <hoangtin285@gmail.com>
2026-07-08 11:15:49 +0700

    build success Schedule_View, but not yet link to navigator. Waiting for fix Main_View

---
+++ src/view/Schedule_View.h
@@ -0,0 +1,58 @@
+#ifndef SCHEDULE_VIEW_H
+#define SCHEDULE_VIEW_H
+#include <QWidget>
+#include <QComboBox>
+#include <QMap>
+#include <QStringList>
+#include <QTableWidgetItem>
+#include <QMessageBox>
+#include <QWidget>
+
+namespace Ui {
+class Schedule_View;
+}
+
+class Schedule_View : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Schedule_View(QWidget *parent = nullptr);
+    ~Schedule_View();
+
+    // for allow registrate, only fixed day to registrate
+    void enableRegistration(bool isEnable);
+
+    // show error when time overlapping
+    void showError(const QString& mess);
+
+    // loadData
+    void setUpDataInputTable(const QList<QString>& listDays, int openTime, int closeTime);
+
+    // data for pre-view table, when add success, controller call this function to update pre-view table. int in map is colums, QList is content of this colum
+    void updateSumTable(const QMap<int, QList<QString>> weeklyData);
+
+    // reset input table, controller call, when add shitf success
+    void resetInputTable();
+
+private:
+    Ui::Schedule_View *ui;
+    void setUpUI();
+    QComboBox* createComboBox(const QList<QString>& item);
+
+signals:
+
+    // request in Schedule page
+    void requestAddShift(const QString& day, const QString& startTime, const QString& endTime);
+    void requestSaveShift();
+
+    // switch tab
+    void profileClicked();
+    void dashboardClicked();
+    void employeeClicked();
+private slots:
+    void buttonAddClicked();
+    void buttonSaveClicked();
+};
+
+#endif // SCHEDULE_VIEW_H
