aa9c23fbcaf9ecd55ab9c3032faf5fe389492795
taan <tanvuthanh313130911@gmail.com>
2026-07-10 17:18:52 +0700

    Update core/Shift, added Schedule_Model

--- src/core/Shift.cpp
+++ src/core/Shift.cpp
@@ -1,12 +1,9 @@
 #include "Shift.h"
 
 
-Shift::Shift(short int id, QDate date, QTime start, QTime end) {
-    this->status = 0; //pending
-    this->EmployeeID = id;
-    this->date = date;
-    this->startTime = start;
-    this->endTime = end;
+Shift::Shift(short int id, QDate date, QTime start, QTime end)
+    : status(0), EmployeeID(id), date(date), startTime(start), endTime(end)
+{
     setDayOfWeek();
 }
 
@@ -15,7 +12,7 @@ void Shift::setDayOfWeek() {
     short int k = date.year() % 100; // last 2 digits of year
     short int j = date.year() / 100; // first 2 digits of year
     if (m < 3) m += 12; // jan: 13, feb: 14
-    this->dayOfWeek = (date.day() + (13 / 5 * (m + 1)) + k + (k / 4) + (j / 4) + (2 * j)) % 7;
+    this->dayOfWeek = (date.day() + ((13 * (m + 1)) / 5) + k + (k / 4) + (j / 4) + (2 * j)) % 7;
     if (this->dayOfWeek == 0) this->dayOfWeek = 7;
 }
 
@@ -30,3 +27,5 @@ void Shift::setTime(QTime start, QTime end) {
     this->startTime = start;
     this->endTime = end;
 }
+
+void Shift::setStatus(short int status) { this->status = status; }
---
+++ src/model/Schedule_Model.h
@@ -0,0 +1,28 @@
+#ifndef SCHEDULE_MODEL_H
+#define SCHEDULE_MODEL_H
+
+#include "core/Shift.h"
+#include <QString>
+#include "utils/Database.h"
+
+
+class Schedule_Model
+{
+private:
+    QList<QList<Shift*>> shiftList{7}; // 7 days
+    // maybe add qlist temporary shift list
+    int numberOfShift;
+public:
+    Schedule_Model();
+    bool checkOverlapping(short int id, QDate date, QTime start, QTime end);
+    Shift* getPreviewShift(short int id, QDate date, QTime start, QTime end);
+    Shift* handleAddShiftSubmission(short int id, QDate date, QTime start, QTime end);
+    void getSchedule(short int id);
+    pair<QDate, QDate> getRangeOfWeek();
+    ~Schedule_Model();
+signals:
+    void handleAddShiftSubmission(); // put this to schedule controller
+    void updateStatusSignal();
+};
+
+#endif // SCHEDULE_MODEL_H
