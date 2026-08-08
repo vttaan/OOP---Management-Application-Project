be355322e754a7fc0c42124f37b268977b56dde6
HwngMP <pmai4118@gmail.com>
2026-08-08 21:38:44 +0700

    fix

--- src/utils/SessionManage.h
+++ src/utils/SessionManage.h
@@ -1,10 +1,13 @@
-#pragma once
+#pragma once
+
 #include "global.h"
 #include "core/User.h"
 
 class SessionManager
 {
 private:
+    SessionManager();
+    ~SessionManager();
 
     SessionManager(const SessionManager &) = delete;
     SessionManager &operator=(const SessionManager &) = delete;
@@ -12,14 +15,11 @@ private:
     User *currentUser = nullptr;
 
 public:
-
-    SessionManager();
-    ~SessionManager();
     static SessionManager *getInstance();
 
     void saveCurrentInfo(User *user);
     void clearInfo();
     User *getCurrentUser() const;
-    //void setCurrentUser(User* user);
+    // void setCurrentUser(User* user);
     bool checkPermission(const QString &requiredRole) const;
 };
